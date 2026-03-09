e0fa64fff605fb98d6e301bb2f7dd8b4a768f880
Carmem Zavattieri <carmemzavattieri@icloud.com>
2026-03-09 16:17:06 +0000

    feat: refine dashboard navbar styling

--- src/app/dashboard/layout/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard/layout/dashboard-layout/dashboard-layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.css'
 })
